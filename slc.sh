6edc62696193e7aebeca7c3d73ebf64d0722a4aa
ninamonopoly <nina.scooby@yahoo.com>
2022-08-18 00:23:32 +0000

    db execution

---
+++ slc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Single line comment stops"
+#This script is for file management
+# You require this script to run in Linux
+# Thank you!
+echo "Single line comment starts"
+date
+
+<<Simon 
+     "Multi-line comment starts"
+echo Thanks for coming 
+echo "Multi-line comments stop"
+
+date
+
